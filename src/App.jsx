8fb1f60897f7b027c00603cd4879b5e32a7bb186
francis-gomes-esq <152029704+francis-gomes-esq@users.noreply.github.com>
2025-07-17 10:46:45 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
 import Home from "./pages/Home";
 import Score from "./components/ScoreComp";
 import Navbar from "./components/Navbar";
